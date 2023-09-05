da576c91876766efd1e989742ffdfdd239f967f1
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2023-09-05 12:28:48 +0700

    Sửa lỗi trả về đăng nhập

--- dboperation.js
+++ dboperation.js
@@ -2,40 +2,76 @@ var config = require("./dbconfig");//require() dùng để tải file hoặc gó
 const sql = require("mssql");//tải module mssql
 const bcrypt = require('bcrypt'); // dùng để mã hoá mật khẩu
 //const { format } = require('date-fns'); //ép định dạng cho ngày tháng năm
-
+//const argon2 = require('argon2');
+//const bcrypt = require('bcryptjs');
+//const crypto = require('crypto');
 
 //xử lý tải dữ liệu xe
 async function layuser() {
   try {
     let pool = await sql.connect(config);
     let res = await pool.request().query("SELECT * FROM users")
-      return res.recordset;
+    return res.recordset;
   } catch (error) {
     console.log("Lỗi Tải Dữ Liệu User :" + error);
-  }finally {
+  } finally {
     sql.close();
   }
 }
 
 
-//xử lý đăng ký
+// Hàm đăng ký
 async function dangKy(data) {
   try {
     let pool = await sql.connect(config);
-    console.log('opera', data);
     // Mã hóa mật khẩu
-    const hashedPassword = await bcrypt.hash(data.Pass, 10); // Số vòng lặp (salt) là 10, bạn có thể điều chỉnh tùy theo yêu cầu
+    //const hashedPassword = hashPassword(data.Pass);
+    const hashedPassword = await bcrypt.hash(data.Pass, 10);
     let res = await pool.request()
       .input('HoTen', sql.NVarChar, data.HoTen)
       .input('UserName', sql.VarChar, data.UserName)
-      .input('Pass', sql.VarChar, hashedPassword) // Truyền mật khẩu đã được mã hóa
+      .input('Pass', sql.NVarChar, hashedPassword) // Truyền mật khẩu đã được mã hóa
       .execute('dangky');
     return res.recordsets;
   } catch (error) {
     console.log("Lỗi khi đăng ký: " + error);
   }
 }
+
+
+// Hàm đăng nhập
+async function dangNhap(data) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool.request()
+      .query("SELECT * FROM users WHERE UserName = '" + data.UserName + "'");
+    console.log("result.recordset:", result.recordset);
+
+    if (result !== undefined && result.recordset.length > 0) {
+      const user = result.recordset[0];
+      //so sánh mật khẩu
+      //const isPasswordMatch = verifyPassword(data.Pass, user.Pass);
+      const isPasswordMatch = await bcrypt.compare(data.Pass, user.Pass);
+      if (isPasswordMatch) {
+        // Đăng nhập thành công
+        return { success: true, message: 'Đăng nhập thành công!' };
+      } else {
+        // Mật khẩu không khớp
+        //return { success: false, message: 'Mật khẩu không đúng!' };
+        return { success: false, message: 'Tài khoản hoặc mật khẩu không chính xác!' };
+      }
+    } else {
+      // Người dùng không tồn tại
+      //return { success: false, message: 'Người dùng không tồn tại!' };
+      return { success: false, message: 'Tài khoản hoặc mật khẩu không chính xác!' };
+    }
+  } catch (error) {
+    console.log("Lỗi khi đăng nhập: " + error);
+    return { success: false, message: 'Đã xảy ra lỗi khi đăng nhập' };
+  }
+}
 module.exports = {
   layuser: layuser,
-  dangKy:dangKy
+  dangKy: dangKy,
+  dangNhap: dangNhap
 };
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 var express = require('express');
-//const cors = require('cors');//lỗi CORS
+const cors = require('cors');//lỗi CORS
 var router = express.Router();
 const sql = require("../dboperation");//load file dboperation
 const bodyParser = require('body-parser');//phương thức POST
 
+router.use(cors());
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'APPLE4CAR' });
 });
 
-//Lấy dữ liệu xe
+//Lấy dữ liệu user
 router.get("/layuser", function (req, res, next) {
   sql.layuser().then((result) => {
     res.json(result);
@@ -21,12 +22,28 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 router.post('/dangky', function (req, res, next) {
   // Lấy dữ liệu được gửi đến từ client
+  console.log(req.body);
   const data = req.body;
   // Thực hiện thêm dữ liệu vào SQL
   sql.dangKy(data).then((result) => {
+    //return { success: true, message: 'Đăng nhập thành công!' };
+    res.status(200).json({ success: true, message: 'Đăng ký thành công!' });
+  }).catch((error) => {
+    res.status(500).json({ success: false, message: 'Lỗi đăng ký', error });
+  });
+});
+
+//đăng nhập
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+router.post('/dangnhap', function (req, res, next) {
+  // Lấy dữ liệu được gửi đến từ client
+  const data = req.body;
+  // Thực hiện thêm dữ liệu vào SQL
+  sql.dangNhap(data).then((result) => {
     res.status(200).json(result);
   }).catch((error) => {
-      res.status(500).json({ error: 'Lỗi khi đăng ký: ', error });
+    res.status(500).json({ error: 'Đã xảy ra lỗi khi đăng nhập:  ', error });
   });
 });
 
