f1fec2f221f9a654df7c536208dbd155ec913d75
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2023-09-09 10:56:43 +0700

    Add files via upload

--- dboperation.js
+++ dboperation.js
@@ -1,41 +1,216 @@
-var config = require("./dbconfig");//require() dùng để tải file hoặc gói
-const sql = require("mssql");//tải module mssql
-const bcrypt = require('bcrypt'); // dùng để mã hoá mật khẩu
+var config = require("./dbconfig"); //require() dùng để tải file hoặc gói
+const sql = require("mssql"); //tải module mssql
+const bcrypt = require("bcrypt"); // dùng để mã hoá mật khẩu
 //const { format } = require('date-fns'); //ép định dạng cho ngày tháng năm
-
+//const argon2 = require('argon2');
+//const bcrypt = require('bcryptjs');
+//const crypto = require('crypto');
 
 //xử lý tải dữ liệu xe
 async function layuser() {
   try {
     let pool = await sql.connect(config);
-    let res = await pool.request().query("SELECT * FROM users")
-      return res.recordset;
+    let res = await pool.request().query("SELECT * FROM users");
+    return res.recordset;
   } catch (error) {
     console.log("Lỗi Tải Dữ Liệu User :" + error);
-  }finally {
+  } finally {
     sql.close();
   }
 }
 
-
-//xử lý đăng ký
+// Hàm đăng ký
 async function dangKy(data) {
   try {
     let pool = await sql.connect(config);
-    console.log('opera', data);
     // Mã hóa mật khẩu
-    const hashedPassword = await bcrypt.hash(data.Pass, 10); // Số vòng lặp (salt) là 10, bạn có thể điều chỉnh tùy theo yêu cầu
-    let res = await pool.request()
-      .input('HoTen', sql.NVarChar, data.HoTen)
-      .input('UserName', sql.VarChar, data.UserName)
-      .input('Pass', sql.VarChar, hashedPassword) // Truyền mật khẩu đã được mã hóa
-      .execute('dangky');
+    //const hashedPassword = hashPassword(data.Pass);
+    const hashedPassword = await bcrypt.hash(data.Pass, 10);
+    let res = await pool
+      .request()
+      .input("HoTen", sql.NVarChar, data.HoTen)
+      .input("UserName", sql.VarChar, data.UserName)
+      .input("Pass", sql.NVarChar, hashedPassword) // Truyền mật khẩu đã được mã hóa
+      .execute("dangky");
     return res.recordsets;
   } catch (error) {
     console.log("Lỗi khi đăng ký: " + error);
+  } finally {
+    sql.close();
   }
 }
+
+// Hàm đăng nhập
+async function dangNhap(data, res) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .query("SELECT * FROM users WHERE UserName = '" + data.UserName + "'");
+
+    if (result !== undefined && result.recordset.length > 0) {
+      let matchedUser;
+      for (const user of result.recordset) {
+        const isPasswordMatch = await bcrypt.compare(data.Pass, user.Pass);
+        if (isPasswordMatch) {
+          matchedUser = user;
+          break;
+        }
+      }
+      if (matchedUser) {
+        console.log("matchedUser: ", matchedUser);
+        const userID = matchedUser.IDUsers; // Lấy ID từ người dùng khớp
+        // Đăng nhập thành công
+        const currentTime = Date.now().toString();
+        const secret = "apple4car"; // Thay đổi chuỗi bí mật thành giá trị thực tế
+        const sessionID = bcrypt.hashSync(currentTime + secret, 10);
+        await sql.query`UPDATE users SET SessionID = ${sessionID} WHERE IDUsers = ${userID}`;
+
+        //res.cookie('ss', sessionID, { maxAge: 3600000, httpOnly: true });
+        //res.cookie('ss2', 'aaaa', { maxAge: oneDay, httpOnly: true, secure: false });
+        return {
+          success: true,
+          message: "Đăng nhập thành công!",
+          cookieValue: sessionID,
+        };
+      } else {
+        // Mật khẩu không khớp
+        return {
+          success: false,
+          message: "Tài khoản hoặc mật khẩu không chính xác!",
+        };
+      }
+    } else {
+      // Người dùng không tồn tại
+      return {
+        success: false,
+        message: "Tài khoản hoặc mật khẩu không chính xác!",
+      };
+    }
+  } catch (error) {
+    console.error("Đã xảy ra lỗi trong quá trình đăng nhập: ", error);
+    return {
+      success: false,
+      message: "Đã xảy ra lỗi trong quá trình đăng nhập!",
+    };
+  } finally {
+    sql.close();
+  }
+}
+
+//hàm lấy thông tin từ sessionID
+async function SessionID(sessionID) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("SessionID", sql.NVarChar, sessionID.sessionID)
+      .query(
+        "SELECT IDUsers, HoTen, UserName,Quyen FROM users WHERE SessionID = @SessionID"
+      );
+
+    if (result.recordset.length === 0) {
+      return { success: false, message: "Bạn hãy đăng nhập lại!" };
+    }
+    return result.recordset;
+  } catch (error) {
+    console.log("Lỗi khi kiểm tra SessionID: " + error);
+  } finally {
+    sql.close();
+  }
+}
+
+//todo: lấy thông tin xe
+async function layxe() {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool.request().query("SELECT * FROM xe");
+    return result.recordset;
+  } catch (error) {
+    console.log("Lỗi Tải Dữ Liệu Xe: " + error);
+  } finally {
+    sql.close();
+  }
+}
+//todo: Thêm thông tin xe
+async function themxe(MaXe, TenXe, MaLoaiXe, BienSo, GhiChu, Anh) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("MaXe", sql.Int, MaXe)
+      .input("TenXe", sql.NVarChar, TenXe)
+      .input("MaLoaiXe", sql.Int, MaLoaiXe)
+      .input("BienSo", sql.VarChar, BienSo)
+      .input("GhiChu", sql.NVarChar, GhiChu)
+      .input("Anh", sql.NVarChar, Anh)
+      .execute("themxe");
+    return result.recordsets;
+  } catch (error) {
+    console.log("Lỗi khi thêm thông tin xe: " + error);
+  }
+}
+//todo: sửa thông tin xe
+async function suaxe(MaXe, TenXe, MaLoaiXe, BienSo, GhiChu, Anh) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("MaXe", sql.Int, MaXe)
+      .input("TenXe", sql.NVarChar, TenXe)
+      .input("MaLoaiXe", sql.Int, MaLoaiXe)
+      .input("BienSo", sql.VarChar, BienSo)
+      .input("GhiChu", sql.NVarChar, GhiChu)
+      .input("Anh", sql.NVarChar, Anh)
+      .execute("suaxe");
+    return result.recordsets;
+  } catch (error) {
+    console.log("Lỗi khi sửa thông tin xe: " + error);
+    res
+      .status(500)
+      .json({ success: false, message: "Lỗi sửa thông tin xe", error });
+  }
+}
+//todo: Xóa thông tin xe
+async function xoaxe(MaXe) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("MaXe", sql.Int, MaXe)
+      .execute("xoaxe");
+    return result;
+  } catch (error) {
+    console.log("Lỗi khi xóa thông tin xe: " + error);
+    throw error;
+  }
+}
+
+//hàm thêm, sửa ảnh xe
+async function themSuaAnhXe(MaXe,DuongDanAnh) {
+  try {
+    let pool = await sql.connect(config);
+    let result = await pool
+      .request()
+      .input("MaXe", sql.Int, MaXe)
+      .input("Anh", sql.VarChar, DuongDanAnh)
+      .query(
+        "UPDATE XE SET Anh = @Anh WHERE MaXe = @MaXe"
+      );
+      return result;
+    } catch (error) {
+      console.log("Lỗi khi thêm ảnh xe: " + error);
+      throw error;
+    }
+}
 module.exports = {
+  layxe: layxe,
   layuser: layuser,
-  dangKy:dangKy
+  dangKy: dangKy,
+  dangNhap: dangNhap,
+  SessionID: SessionID,
+  suaxe: suaxe,
+  themxe: themxe,
+  xoaxe: xoaxe,
+  themSuaAnhXe:themSuaAnhXe
 };
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,175 @@
-var express = require('express');
-//const cors = require('cors');//lỗi CORS
+var express = require("express");
+const cors = require("cors"); //xử lý chính sách CORS
 var router = express.Router();
-const sql = require("../dboperation");//load file dboperation
-const bodyParser = require('body-parser');//phương thức POST
+const sql = require("../dboperation"); //load file dboperation
+const bodyParser = require("body-parser"); //phương thức POST
+//const bcrypt = require("bcrypt"); //dùng để tạo phiên đăng nhập
+const multer = require('multer');//xử lý lưu ảnh
+const path = require('path');//xử lý thêm phần mở rộng cho ảnh
 
+router.use(cors());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'APPLE4CAR' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "APPLE4CAR" });
 });
 
-//Lấy dữ liệu xe
+//Lấy dữ liệu user
 router.get("/layuser", function (req, res, next) {
   sql.layuser().then((result) => {
     res.json(result);
   });
 });
+//todo Lấy dữ liệu xe
 
-//Đăng ký hệ thống 
+router.get("/layxe", function (req, res, next) {
+  sql.layxe().then((result) => {
+    res.json(result);
+  });
+});
+//Đăng ký hệ thống
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-router.post('/dangky', function (req, res, next) {
+router.post("/dangky", function (req, res, next) {
+  // Lấy dữ liệu được gửi đến từ client
+  console.log(req.body);
+  const data = req.body;
+  // Thực hiện thêm dữ liệu vào SQL
+  sql
+    .dangKy(data)
+    .then((result) => {
+      //return { success: true, message: 'Đăng nhập thành công!' };
+      res.status(200).json({ success: true, message: "Đăng ký thành công!" });
+    })
+    .catch((error) => {
+      res.status(500).json({ success: false, message: "Lỗi đăng ký", error });
+    });
+});
+
+//đăng nhập
+
+router.post("/dangnhap", function (req, res, next) {
   // Lấy dữ liệu được gửi đến từ client
   const data = req.body;
   // Thực hiện thêm dữ liệu vào SQL
-  sql.dangKy(data).then((result) => {
-    res.status(200).json(result);
-  }).catch((error) => {
-      res.status(500).json({ error: 'Lỗi khi đăng ký: ', error });
-  });
+  sql
+    .dangNhap(data, res)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Đã xảy ra lỗi khi đăng nhập:  ", error });
+    });
 });
 
+//kiểm tra phiên làm việc:
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+router.post("/sessionID", function (req, res, next) {
+  // Lấy dữ liệu được gửi đến từ client
+  const data = req.body;
+  // Thực hiện thêm dữ liệu vào SQL
+  sql
+    .SessionID(data)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Đã xảy ra lỗi khi đăng nhập:  ", error });
+    });
+});
+
+//*quản lý xe
+
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+//! Thêm thông tin xe
+router.post("/themxe", function (req, res, next) {
+  const { MaXe, TenXe, MaLoaiXe, BienSo, GhiChu, Anh } = req.body;
+  sql
+    .themxe(MaXe, TenXe, MaLoaiXe, BienSo, GhiChu, Anh)
+    .then(() => {
+      res
+        .status(200)
+        .json({ success: true, message: "Thêm thông tin xe thành công" });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ success: false, message: "Lỗi thêm thông tin xe", error });
+    });
+});
+//* Sửa thông tin xe
+router.put("/suaxe/:MaXe", function (req, res, next) {
+  const MaXe = parseInt(req.params.MaXe);
+
+  const { TenXe, MaLoaiXe, BienSo, GhiChu, Anh } = req.body;
+
+  sql
+    .suaxe(MaXe, TenXe, MaLoaiXe, BienSo, GhiChu, Anh)
+    .then(() => {
+      res
+        .status(200)
+        .json({ success: true, message: "Sửa thông tin xe thành công" });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ success: false, message: "Lỗi sửa thông tin xe", error });
+    });
+});
+
+//? Xóa thông tin xe
+router.delete("/xoaxe/:MaXe", function (req, res, next) {
+  const MaXe = parseInt(req.params.MaXe);
+  sql
+    .xoaxe(MaXe)
+    .then(() => {
+      res
+        .status(200)
+        .json({ success: true, message: "Xóa thông tin xe thành công" });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ success: false, message: "Lỗi xóa thông tin xe", error });
+    });
+});
+
+// Upload ảnh xe
+router.use(express.static(path.join(__dirname, 'public')));//thiết lập cho phép truy cập file static
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploadAnhXe/'); // Thư mục lưu trữ file
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname); // Lấy phần mở rộng của file
+    cb(null, file.fieldname + '-' + Date.now() + ext); // Đổi tên file và thêm phần mở rộng
+  }
+});
+
+const newupload = multer({ storage: storage });
+router.post('/uploadAnhXe/:MaXe', newupload.single('image'), (req, res) => {
+  const MaXe = req.params.MaXe;
+  const imagePath = req.file.path;
+  //const domain = 'http://localhost:3000';
+  const domain = 'http://' + req.hostname + ':' + req.app.get('port');
+  const newPath = path.relative('public', imagePath);
+  const imagePathWithDomain = path.join(domain,newPath);
+
+  sql
+    .themSuaAnhXe(MaXe,imagePathWithDomain)
+    .then(() => {
+      res
+        .status(200)
+        .json({ success: true, message: "Cập nhật ảnh xe thành công" });
+    })
+    .catch((error) => {
+      res
+        .status(500)
+        .json({ success: false, message: "Lỗi khi Cập nhật ảnh xe", error });
+    });
+  
+});
 module.exports = router;
