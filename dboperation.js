7fa4a934023084fe13fba71a9fd508cfaa897c82
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2023-10-24 17:05:19 +0700

    Update dboperation.js
    Tìm kiếm update

--- dboperation.js
+++ dboperation.js
@@ -609,6 +609,27 @@ async function capNhatNoiDungBaiViet(MaBai, updatedContent) {
     
 }
 
+
+//Xử lý tìm kiếm dữ liệu: 
+async function searchData(TenBang, columnName, Search) {
+  try {
+    // Giải mã chuỗi tìm kiếm từ URL encoding
+    const decodedSearch = decodeURIComponent(Search);
+    let pool = await sql.connect(config);
+    const query = `SELECT * FROM ${TenBang} WHERE ${columnName} LIKE @Search`;
+    let res = await pool.request()
+      .input('Search', sql.NVarChar(50), `%${decodedSearch}%`)
+      .query(query);
+    if (res.recordset.length > 0) {
+      return res.recordset;
+    } else {
+      const errorMessage = `Không tìm thấy ${decodedSearch} trong cột ${columnName}`;
+      return { success: false, message: errorMessage };
+    }
+  } catch (error) {
+    throw error;
+  }
+}
 module.exports = {
   layxe: layxe,
   layuser: layuser,
