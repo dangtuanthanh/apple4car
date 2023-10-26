de0f528501e6a99772032c9a1a60389a6bc9df96
Tín Lưu <107022820+tinluuVTL@users.noreply.github.com>
2023-10-26 15:40:29 +0700

    Thêm api hiển thị bài viết lên trang chủ

--- routes/index.js
+++ routes/index.js
@@ -483,6 +483,37 @@ router.get('/timkiem/:TenBang/:ColumnName/:Search', function (req, res, next) {
     res.sendStatus(500);
   }
 });
+
+
+//todo API hiển thị bài viết lên trang chủ
+
+router.get("/posts", function (req, res, next) {
+  sql.hienthibaiviet().then((result) => {
+    res.json(result);
+  });
+});
+
+router.get("/layuserid/:id", function (req, res, next) {
+  const IDUsers = req.params.id; // Lấy id contact từ tham số đường dẫn
+
+  sql
+    .layuserid(IDUsers)
+    .then((result) => {
+      if (result.length > 0) {
+        res.json(result[0]); // Chỉ xuất ra dữ liệu của phần tử đầu tiên trong kết quả
+      } else {
+        res
+          .status(404)
+          .json({ error: "Không tìm thấy thông tin liên hệ với id " + IDUsers });
+      }
+    })
+    .catch((error) => {
+      console.log("Lỗi Tải Dữ Liệu Contact: " + error);
+      res.status(500).json({ error: "Lỗi Tải Dữ Liệu Contact" });
+    });
+});
+
+
 module.exports = router;
 
 
